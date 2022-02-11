b4c238e6f3fd77e750bb97cc4fb30a15c28cb59d
LarisaZaripova <larik7lolik@gmail.com>
2022-02-11 16:40:25 +0300

    added Example 28

---
+++ Example_28/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 28: Подсчитать сумму цифр в числе.
+Console.WriteLine("Введите число: ");
+string str = Console.ReadLine();
+int num = int.Parse(str);
+ //Console.WriteLine(str.Length);
+  int i = 0;
+ int Sum = 0;
+ while(num!= 0 && i <= str.Length)
+ {
+     num = num / 10;
+     Sum = num + i;
+     i++;
+     
+ }
+ Console.WriteLine(Sum);
