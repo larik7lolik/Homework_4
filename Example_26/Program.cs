c18c58395b74963db91d79b1d3f9adf20494dd7e
LarisaZaripova <larik7lolik@gmail.com>
2022-02-11 12:36:49 +0300

    added example 26

---
+++ Example_26/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 26: Возведите число А в натуральную степень В используя цикл
+Console.WriteLine("Введите число: ");
+int A = int.Parse(Console.ReadLine());
+int B = A * A * A * A * A ;
+int result = A*B;
+for ( A = 1; B >=0; B++)
+{
+    Console.WriteLine(result);
+    break;
+
+}
+
