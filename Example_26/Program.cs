e3687f50a1bcd7f33fc0feea691dec8c16f177ed
LarisaZaripova <larik7lolik@gmail.com>
2022-02-17 08:23:09 +0300

    added example 26

--- Example_26/Program.cs
+++ Example_26/Program.cs
@@ -1,5 +1,16 @@
 ﻿// Задача 26: Возведите число А в натуральную степень В используя цикл
 Console.Clear();
+int A = new Random().Next(1,5);
+int B = new Random().Next(1,3);
+double result = 0;
+while(B >= 0)
+{
+   result = Math.Pow(A,B);//возведение числа А в степень В
+   Console.WriteLine($"{A} ^ {B} = {result}");
+   break;
+}
+
+/*Console.Clear();
 Console.WriteLine();
 int a = new Random().Next(1,10);
 int b = new Random().Next(1,10);
@@ -12,5 +23,5 @@ double exp(int basis, int extent)
     else return basis*exp(basis, extent-1);
 }
 Console.Write($"Введено значение А = {a}  и B = {b}");
-Console.WriteLine($"  {a} возведенное в степень {b} равно {exp(a,b)}");
+Console.WriteLine($"  {a} возведенное в степень {b} равно {exp(a,b)}"); */
 
