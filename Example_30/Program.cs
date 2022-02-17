37a72e7765bb916782d8f60b95036b1a946b7045
LarisaZaripova <larik7lolik@gmail.com>
2022-02-17 09:39:22 +0300

    added Example 30

---
+++ Example_30/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 30: Показать кубы чисел, заканчивающихся на четную цифру.
+/* Console.Clear(); 
+for(int i = 2; i < 10; i+=2)
+{
+    Console.WriteLine(i * i * i);
+} */
+
+Console.Clear(); 
+int n = 10;
+int[] mass = new int[n/2];
+int j = 2;
+for(int i = 0; i < n/2; i++)
+{
+    Console.WriteLine(j * j * j);
+    mass[i] = j*j*j;
+    j+=2;
+} 
